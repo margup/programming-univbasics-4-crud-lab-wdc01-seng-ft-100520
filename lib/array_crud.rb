b9f9c62bb1f26dcbe8df9cf17d07a011c34af9d5
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-14 01:16:39 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,54 @@
+$arr = []
 def create_an_empty_array
+  arr = Array.new
   
 end
 
 def create_an_array
-  
+  $arr = Array.new(4)
 end
 
 def add_element_to_end_of_array(array, element)
   
+  $arr << element
+  
 end
 
 def add_element_to_start_of_array(array, element)
+  $arr.unshift(element)
+  
   
 end
 
 def remove_element_from_end_of_array(array)
+   $arr.pop 
   
 end
 
 def remove_element_from_start_of_array(array)
+  $arr.shift
+  
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  $arr = array[index_number]
+  
   
 end
 
 def retrieve_first_element_from_array(array)
+  array[0]
+  
   
 end
 
 def retrieve_last_element_from_array(array)
+  array[-1]
   
 end
 
 def update_element_from_index(array, index_number, element)
+  array[index_number] = element
 
 end
